b8c289534db0498e6514ecf628d819eed4de3488
kanker2 <karce@ucm.es>
2023-04-18 18:07:41 +0200

    primer commit

---
+++ src/practicando/ClientSocketListener.java
@@ -0,0 +1,43 @@
+package practicando;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientSocketListener extends Thread{
+	
+	Client c;
+	int id;
+	
+	public ClientSocketListener(Client c, int id)
+	{
+		this.c = c;
+		this.id = id;
+	}
+	
+	private void printErrMessage() {
+		System.err.println("Error at SocketListener " + id + " of " + c.getId() + " Client");
+	}
+	
+	@Override
+	public void run() {
+		try {
+			Socket s = new Socket(c.getIP(), c.getPort());
+			PrintWriter foutC = new PrintWriter(s.getOutputStream());
+			foutC.println(c.getFileName());
+			foutC.flush();
+
+			ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+			File fileAsked = (File) ois.readObject();
+			c.updateFile(fileAsked);
+		}
+		catch(IOException e) {
+			printErrMessage();
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			printErrMessage();
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/practicando/Main.java
@@ -0,0 +1,21 @@
+package practicando;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Thread tS = new Thread(() -> Server.f1()),
+			   tC = new Thread(() -> Client.f1());
+		
+//		tS.start();
+//		tC.start();
+		
+		Server s = new Server(1300);
+		Client c = new Client(0, InetAddress.getLocalHost(), 1300, "a.txt");
+//		for(int i = 0; i < 2; i++) {
+//			Client c = new Client(i, InetAddress.getLocalHost(), 1300, "a.txt");
+//		}
+	}
+}
